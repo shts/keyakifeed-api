94df1e9b5e4e87c4602c9c7f3e06862d3485e06d
shts <shts.dev@gmail.com>
2016-07-03 18:34:04 +0900

    check whether new entry

--- crawler_matomefeeds.rb
+++ crawler_matomefeeds.rb
@@ -8,7 +8,7 @@ require "./app"
 
 ROUTINE_TIME = 60 * 30 #30分
 
-url_list = [
+URL_LIST = [
   "http://keyakizaka46ch.jp",
   "http://keyakizaka1.blog.jp",
   "http://keyakizakamatome.blog.jp",
@@ -29,26 +29,33 @@ def normalize str
   str.gsub(/(\r\n|\r|\n|\f)/,"").strip
 end
 
+def is_new? entry_url
+  Api::Matome.where('entry_url = ?', entry_url).first == nil
+end
+
 def fetch_rss_feed
-  url_list.each do |site_url|
+  URL_LIST.each do |site_url|
     feed_urls = Feedbag.find site_url
     feed = Feedjira::Feed.fetch_and_parse feed_urls.first
-
     feed.entries.each do |entry|
-      matome = Api::Matome.new
-      matome[:feed_title] = feed.title
-      matome[:feed_url] = feed.url
-      matome[:entry_title] = entry.title
-      matome[:entry_url] = entry.url
-      matome[:entry_published] = entry.published
-      matome[:entry_categories] = entry.categories
-      matome.save
+      if is_new? entry.url then
+        matome = Api::Matome.new
+        matome[:feed_title] = feed.title
+        matome[:feed_url] = feed.url
+        matome[:entry_title] = entry.title
+        matome[:entry_url] = entry.url
+        matome[:entry_published] = entry.published
+        matome[:entry_categories] = entry.categories
+        matome.save
+      else
+        puts "already saved url -> #{entry.url}"
+      end
     end
   end
 end
 
 EM.run do
-  EM::PeriodicTimer.new(ROUTINE_TIME) do
+  EM::PeriodicTimer.new(10) do
     fetch_rss_feed
   end
 end
